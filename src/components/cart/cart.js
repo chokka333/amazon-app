cc99d4b0933d1d090235301119439696ad720e52
chokka33 <chokka333@gmail.com>
2023-11-11 20:47:08 +0530

    hi

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { useSelector } from 'react-redux';
 import CartItem from './CartItems';
@@ -9,20 +8,30 @@ const Cart = () => {
   const navigate = useNavigate();
 
   const handleClick = () => {
-
     navigate('/');
   };
+
+  const totalAmount = cart.reduce((total, item) => total + item.price * item.quantity, 0);
 
   return (
     <div className="container mt-4">
       <h2>Your Cart</h2>
-      {cart.length!==0?( cart.map((item) => (
-        <CartItem key={item.id} item={item} />
-      ))):(<div className='align-content-center'><p>Cart Empty</p></div>)}
-      <div className="d-flex justify-content-end align-items-end">
-    <button className='btn btn-danger' onClick={handleClick}>Back to home</button>
-</div>
-     
+      {cart.length !== 0 ? (
+        <div>
+          {cart.map((item) => (
+            <CartItem key={item.id} item={item} />
+          ))}
+          <div className="d-flex justify-content-between align-items-center">
+            <p>Total Amount: ${totalAmount}</p>
+            <button className='btn btn-danger' onClick={handleClick}>Back to home</button>
+          </div>
+        </div>
+      ) : (
+        <div className='align-content-center'>
+          <p>Cart Empty</p>
+          <button className='btn btn-danger' onClick={handleClick}>Back to home</button>
+        </div>
+      )}
     </div>
   );
 };
