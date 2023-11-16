5594aabdc15c08f8a80047ffce078cf19a8fc4eb
chokka333 <chokka333@gmail.com>
2023-11-16 11:45:39 +0530

    ji

--- src/components/product.js
+++ src/components/product.js
@@ -1,9 +1,12 @@
-
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { addToCart, increaseQuantity, decreaseQuantity } from '../Actions/actionTypes';
-import "./product.css"
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import {
+  addToCart,
+  increaseQuantity,
+  decreaseQuantity,
+} from "../Actions/actionTypes";
+import "./product.css";
 
 const Product = ({ product }) => {
   const dispatch = useDispatch();
@@ -22,32 +25,44 @@ const Product = ({ product }) => {
   };
 
   const isProductInCart = cart.some((item) => item.id === product.id);
-  const productQuantityInCart = isProductInCart ? cart.find((item) => item.id === product.id).quantity : 0;
+  const productQuantityInCart = isProductInCart
+    ? cart.find((item) => item.id === product.id).quantity
+    : 0;
+
+ 
+  const titleTrim = (title, maxLength) => {
+    return title.length > maxLength ? `${title.substring(0, maxLength)}...` : title;
+  };
 
   return (
     <div className="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-4">
       <div className="card">
-        <Link to={`/product/${product.id}`} className="text-decoration-none">
+        <Link to={`/product/${product.id}`}>
           <img
             src={product.image}
-            className="card-img-top image-css"
+            className="card-img-top product-image"
             alt={product.title}
-            
           />
         </Link>
         <div className="card-body">
-          <h5 className="card-title">{product.title}</h5>
+          <h5 className="card-title">{titleTrim(product.title, 20)}</h5>
           <p className="card-text">Price: ${product.price}</p>
           <p className="card-text">
             Rating: {product.rating.rate} ({product.rating.count} reviews)
           </p>
           {isProductInCart && (
             <div className="d-flex justify-content-between align-items-center mb-2">
-              <button className="btn btn-secondary" onClick={handleDecreaseQuantity}>
+              <button
+                className="btn btn-secondary"
+                onClick={handleDecreaseQuantity}
+              >
                 -
               </button>
               <span className="font-weight-bold">{productQuantityInCart}</span>
-              <button className="btn btn-secondary" onClick={handleIncreaseQuantity}>
+              <button
+                className="btn btn-secondary"
+                onClick={handleIncreaseQuantity}
+              >
                 +
               </button>
             </div>
@@ -56,7 +71,7 @@ const Product = ({ product }) => {
             className="btn btn-primary"
             onClick={isProductInCart ? handleIncreaseQuantity : handleAddToCart}
           >
-            {isProductInCart ? 'Add More' : 'Add to Cart'}
+            {isProductInCart ? "Add More" : "Add to Cart"}
           </button>
         </div>
       </div>
